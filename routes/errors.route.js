45b2c547fc73e0b2f1081bf75c616fbcfd3feed8
Jesulonimii <jesulonimii.will@gmail.com>
2023-08-06 14:52:59 +0100

    fix: corrected error routes

--- routes/errors.route.js
+++ routes/errors.route.js
@@ -5,12 +5,12 @@ import { ErrorResponse, STATUS_CODE } from "#utils";
 
 const router = Router();
 
-router.all("/", (req, res) => {
+router.get("/", (req, res) => {
 	res.end("Welcome to HealthDB Api");
 })
 
 router.all("/*", (req, res) => {
-	res.status(STATUS_CODE.NOT_FOUND).send(ErrorResponse("Endpoint does not exist."))
+	res.status(STATUS_CODE.NOT_FOUND).send(ErrorResponse(`Endpoint for the specified request '${req.method.toUpperCase()}' does not exist.`))
 })
 
 export default router;
