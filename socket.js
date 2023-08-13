51b704ca688b83f53c9f656e6ad912c22ccce59d
Jesulonimii <jesulonimii.will@gmail.com>
2023-08-13 06:44:30 +0100

    chore: added localhost to socket cors

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 import { Server } from "socket.io";
 
-const allowedOrigins = ["http://localhost:3000", "https://healthdb-admin.vercel.app"];
+const allowedOrigins = ["http://localhost:3000", "https://healthdb-admin.vercel.app", "http://192.168.0.101:3000"];
 
 export const attachSocket = (server, app) => {
 
